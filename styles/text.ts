5c752b4bcb5fcdeab43ba2cc1da524913662c816
Calvin Teixeira <calvin.teixeira@avmb.com.br>
2024-06-24 17:00:37 -0300

    Criando arquivo para estilos globais de textos

---
+++ styles/text.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const globalTextStyles = StyleSheet.create({
+    titlePage: {
+        fontSize: 22,
+        fontWeight: 700
+    }
+})
+
+export { globalTextStyles }
