b4386ee46265ec4d443719ac13b628c6c5e71a9d
Calvin <calvingsx@gmail.com>
2024-06-26 08:30:10 -0300

    Reduzindo número de registros nos dados

--- data/db.ts
+++ data/db.ts
@@ -21,7 +21,7 @@ const dbHumorLevel: IHumorLevel[] = [
 const dbAnnotation: IAnnotation[] = [
   {
     id: "1",
-    createdAt: "24/06/2024",
+    createdAt: "01/01/2024",
     description:
       "Hoje eu acordei um pouco mal, acabei esquecendo do meu lanche da manhã e fui para o trabalho sem comer. Consegui ser produtivo, mas estava muito cansado do dia anterior. Ao chegar do trabalho pude descansar melhor e estudar alguns assuntos pendentes.",
     humorLevel: 2,
@@ -29,76 +29,12 @@ const dbAnnotation: IAnnotation[] = [
   },
   {
     id: "2",
-    createdAt: "22/06/2024",
+    createdAt: "20/06/2024",
     description:
       "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
     humorLevel: 3,
     resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "3",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "4",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "5",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "6",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "7",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "8",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "9",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
-  {
-    id: "10",
-    createdAt: "22/06/2024",
-    description:
-      "Hoje eu acordei bem, tomei um banho e café da manhã bem cedo, logo após minha aula da faculdade. Fui super produtivo, o dia estava bonito, com bastante sol porém com um clima agradável, nem frio e nem calor. Durante a noite continuei meus projetos e fui até a sorveteria comer um açaí com minha esposa, depois assistimos Harry Potter.",
-    humorLevel: 3,
-    resume: "Harry Potter e Açaí",
-  },
+  } 
 ];
 
 export { dbAnnotation, dbHumorLevel };
