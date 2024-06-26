9ea1a2a59de8d9dba3f8f6835623ec1f8136530d
Calvin <calvingsx@gmail.com>
2024-06-26 16:13:57 -0300

    Ajustando alguns textos

--- app/(tabs)/annotations/index.tsx
+++ app/(tabs)/annotations/index.tsx
@@ -1,49 +1,49 @@
-import { FlatList, View, Alert } from "react-native";
-import { Text, Skeleton, Button, useTheme } from "@rneui/themed";
-import React from "react";
-import { MainContainer, PreviewAnnotation, SelectBox } from "@/components";
-import { globalTextStyles } from "@/styles/text";
-import { Plus } from "@/icons";
-import { dbMonths, dbYears } from "@/data/auxData";
-import { useAnnotationData } from "@/store";
-import { IAnnotation } from "@/data/types";
+import { FlatList, View, Alert, Modal } from 'react-native'
+import { Text, Skeleton, Button, useTheme } from '@rneui/themed'
+import React from 'react'
+import { MainContainer, PreviewAnnotation, SelectBox, CustomModal } from '@/components'
+import { globalTextStyles } from '@/styles/text'
+import { Plus } from '@/icons'
+import { dbMonths, dbYears } from '@/data/auxData'
+import { useAnnotationData } from '@/store'
+import { IAnnotation } from '@/data/types'
 
 export default function Index() {
-  const annotations = useAnnotationData((state) => state);
-  const { theme } = useTheme();
-  const [monthFilterValue, setMonthFilterValue] = React.useState(dbMonths[0]);
-  const [yearsFilterValue, setYearsFilterValue] = React.useState(dbYears[0]);
-  const [loadingData, setLoadingData] = React.useState<boolean>(true);
+  const annotations = useAnnotationData((state) => state)
+  const [monthFilterValue, setMonthFilterValue] = React.useState(dbMonths[0])
+  const [yearsFilterValue, setYearsFilterValue] = React.useState(dbYears[0])
+  const [loadingData, setLoadingData] = React.useState<boolean>(true)
+  const [visibleModal, setVisibleModal] = React.useState<boolean>(false)
 
   React.useEffect(() => {
     setTimeout(() => {
-      setLoadingData(false);
-    }, 3500);
-  }, []);
+      setLoadingData(false)
+    }, 3500)
+  }, [])
 
   function simulateRequest(
     callBack: () => void,
     timerOpts?: { timeUntilRender: number }
   ) {
-    setLoadingData(true);
+    setLoadingData(true)
     setTimeout(() => {
-      setLoadingData(false);
-      callBack();
-    }, timerOpts?.timeUntilRender || 3500);
+      setLoadingData(false)
+      callBack()
+    }, timerOpts?.timeUntilRender || 3500)
   }
 
-  function handleDeleteAnnotation(annotationId: IAnnotation["id"]) {
+  function handleDeleteAnnotation(annotationId: IAnnotation['id']) {
     Alert.alert(
-      "Deletar anotação",
-      "Tem certeza que deseja deletar essa anotação?",
+      'Deletar anotação',
+      'Tem certeza que deseja deletar essa anotação?',
       [
         {
-          text: "Cancelar",
-          style: "cancel",
+          text: 'Cancelar',
+          style: 'cancel',
         },
         {
-          text: "Deletar",
-          style: "destructive",
+          text: 'Deletar',
+          style: 'destructive',
           onPress: () =>
             simulateRequest(() => annotations.deleteAnnotation(annotationId), {
               timeUntilRender: 1000,
@@ -51,67 +51,61 @@ export default function Index() {
         },
       ],
       { cancelable: false }
-    );
+    )
   }
 
   return (
     <MainContainer>
+      <CustomModal title={'Novo processo'} visible={visibleModal} onRequestClose={() => setVisibleModal(false)}>
+        <Text> Algum conteúdo aqui</Text>
+      </CustomModal>
       <View style={{ flex: 1, paddingTop: 30 }}>
         <Text style={globalTextStyles.titlePage}>Meus processos</Text>
-        <Text style={{ marginTop: 6 }}>
-          Aqui você encontra todas as suas anotações, filtre pelo ano e mês se
-          preferir.
-        </Text>
-        <View style={{ flexDirection: "row", gap: 10 }}>
-          <View
-            style={{ flex: 1, marginTop: 26, flexDirection: "column", gap: 6 }}
-          >
+        <Text style={{ marginTop: 6 }}>Aqui você encontra todas as suas anotações, filtre pelo ano e mês se preferir.</Text>
+        <View style={{ flexDirection: 'row', gap: 10 }}>
+          <View style={{ flex: 1, marginTop: 26, flexDirection: 'column', gap: 6 }}>
             <Text>Filtre pelo ano</Text>
             <SelectBox
               data={dbYears}
               value={yearsFilterValue}
               placeholder="Selecionar ano"
               onChange={(item) => {
-                setYearsFilterValue(item);
+                setYearsFilterValue(item)
               }}
             />
           </View>
-          <View
-            style={{ flex: 1, marginTop: 26, flexDirection: "column", gap: 6 }}
-          >
+          <View style={{ flex: 1, marginTop: 26, flexDirection: 'column', gap: 6 }}>
             <Text>Filtre pelo mês</Text>
             <SelectBox
               data={dbMonths}
               value={monthFilterValue}
               placeholder="Selecionar mês"
               onChange={(item) => {
-                setMonthFilterValue(item);
+                setMonthFilterValue(item)
               }}
             />
           </View>
         </View>
         <Button
+          onPress={() => setVisibleModal(true)}
           type="solid"
-          titleStyle={{ color: "#3d93e3" }}
+          titleStyle={{ color: '#3d93e3' }}
           buttonStyle={{
             marginTop: 14,
-            backgroundColor: "transparent",
-            borderColor: "#3d93e3",
+            backgroundColor: 'transparent',
+            borderColor: '#3d93e3',
             borderWidth: 1,
           }}
         >
-          <Plus size={20} color={"#3d93e3"} /> Nova anotação
+          <Plus size={20} color={'#3d93e3'} /> Novo processo
         </Button>
         <Text style={{ marginTop: 20, marginBottom: 12 }}>
-          Confira suas anotações do mês de{" "}
-          <Text style={{ fontWeight: 700 }}>{monthFilterValue.label}</Text>:
+          Confira suas anotações do mês de <Text style={{ fontWeight: 700 }}>{monthFilterValue.label}</Text>:
         </Text>
         {loadingData ? (
-          <View style={{ flexDirection: "column", gap: 10 }}>
+          <View style={{ flexDirection: 'column', gap: 10 }}>
             {Array.from({ length: 15 }).map((_, index) => {
-              return (
-                <Skeleton key={index} style={{ opacity: 0.5 }} height={30} />
-              );
+              return <Skeleton key={index} style={{ opacity: 0.5 }} height={30} />
             })}
           </View>
         ) : (
@@ -121,18 +115,11 @@ export default function Index() {
             keyExtractor={(item) => item.id}
             ItemSeparatorComponent={() => <View style={{ height: 10 }}></View>}
             renderItem={({ item }) => (
-              <PreviewAnnotation
-                id={item.id}
-                createdAt={item.createdAt}
-                description={item.description}
-                humorLevel={item.humorLevel}
-                resume={item.resume}
-                onDelete={handleDeleteAnnotation}
-              />
+              <PreviewAnnotation id={item.id} createdAt={item.createdAt} description={item.description} humorLevel={item.humorLevel} resume={item.resume} onDelete={handleDeleteAnnotation} />
             )}
           />
         )}
       </View>
     </MainContainer>
-  );
+  )
 }
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import MainContainer from "./container/MainContainer";
 import PreviewAnnotation from "./container/PreviewAnnotation";
 import SelectBox from "./inputs/SelectBox";
+import CustomModal from "./container/CustomModal";
 
-export { MainContainer, PreviewAnnotation, SelectBox }
+export { MainContainer, PreviewAnnotation, SelectBox, CustomModal }
