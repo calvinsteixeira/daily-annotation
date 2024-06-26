3624c1560ca4bc02429cb0ad880702ca3238328b
Calvin <calvingsx@gmail.com>
2024-06-26 08:27:40 -0300

    Adicionando ThemeProvider

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,23 +2,24 @@ import { StyleSheet, Text, View } from "react-native";
 import { StickyNote } from "@/icons/index";
 import { Tabs } from "expo-router";
 import React from "react";
+import { useTheme } from "@rneui/themed";
 
 const _layout = () => {
-
+  const { theme } = useTheme();
   return (
     <Tabs
       screenOptions={{
-        headerShown: false,        
+        headerShown: false,
         tabBarShowLabel: false,
       }}
     >
       <Tabs.Screen
         name="annotations"
         options={{
-          tabBarIcon: ({ color }) => <StickyNote color={color} />,
-          headerShown: false
+          tabBarIcon: () => <StickyNote color={theme.colors.primary} />,
+          headerShown: false,
         }}
-      />      
+      />
     </Tabs>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import { ThemeProvider, createTheme } from "@rneui/themed";
 
+const theme = createTheme({
+  lightColors: {
+    primary: "#FF7A0F",
+  },
+  darkColors: {
+    primary: "blue",
+  }, 
+});
 export default function RootLayout() {
-  return <Stack screenOptions={{ gestureEnabled: false, headerShown: false }}></Stack>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Stack
+        screenOptions={{ gestureEnabled: false, headerShown: false }}
+      ></Stack>
+    </ThemeProvider>
+  );
 }
